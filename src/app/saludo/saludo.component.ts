dbdcea84c7b5787924a7ec109bc6f888a495aa8f
jessicaly <jessiclyaruiz@hotmail.com>
2023-06-13 23:25:05 -0500

    angular hola mundo

---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+ 
+  styleUrls: ['./saludo.component.css'],
+  template:`
+  <h1>hello world</h1>
+  <p>desde Angular</p>
+`
+})
+export class SaludoComponent {
+
+}
